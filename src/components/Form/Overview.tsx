3061a3ee096049e3243bdffb3dd693ea6a23f0c7
Vetle Lindbråten <vetlelind@gmail.com>
2025-02-03 11:36:03 +0100

    Made name and answers visible for Overview view, and started styling it

--- src/components/Form/Overview.tsx
+++ src/components/Form/Overview.tsx
@@ -1,8 +1,10 @@
 import { useConfigState } from "@context/Config";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 const Overview: FC = () => {
     const [config, setConfig] = useConfigState();
+    const [questions, setQuestions] = useState<any[]>([]);
+    const [answerText, setAnswerText] = useState<any>({});
 
     if (!config) return null;
 
@@ -10,10 +12,76 @@ const Overview: FC = () => {
         const jsonData = JSON.parse(config.value!);
 
         console.log(jsonData);
+
+        return jsonData;
     }, [config.value]);
 
-    return <div>
+    const answers = useMemo(() => {
+        const jsonData = JSON.parse(config.answerData || '{}');
 
+        console.log(jsonData);
+
+        return jsonData;
+    }, [config.answerData]);
+
+    useEffect(() => {
+        if (!answers || !questions) return;
+
+        let newAnswers = {} as any;
+
+        questions.forEach((question) => {
+            if (!answers[question.name]) {
+            } else if (question.choices) {
+                const choice = question.choices.find((choice: any) => {
+                    if (typeof choice === "string") {
+                        return choice === answers[question.name]
+                    } else {
+                        return choice.value === answers[question.name]
+                    }
+                });
+
+                newAnswers[question.name] = typeof choice === "string" ? choice : choice.text
+            } else {
+                newAnswers[question.name] = answers[question.name];
+            }
+        });
+
+        setAnswerText(newAnswers)
+    }, [answers, questions]);
+
+    const getNestedQuestions = (elements: any[]) => {
+        let questions: any[] = [];
+
+        elements.forEach((el) => { 
+            if (el.elements) {
+                questions = questions.concat(getNestedQuestions(el.elements));
+            } else {
+                questions.push(el);
+            } 
+        });
+
+        return questions;
+    };
+
+    useEffect(() => {
+        if (!survey) return;
+        const newQuestions: any[] = [];
+
+        survey.pages.forEach((page: any) => {
+            newQuestions.push(...getNestedQuestions(page.elements));
+        });
+
+        setQuestions(newQuestions);
+    }, [survey]);
+
+    console.log(questions);
+
+    return <div className="overview">
+        {questions.map((question, index) => {
+            return <div key={index}>
+                <p>{question.title ? question.title + ": " : ""}{answerText[question.name]}</p>
+            </div>
+        })}
     </div>
 }
 
