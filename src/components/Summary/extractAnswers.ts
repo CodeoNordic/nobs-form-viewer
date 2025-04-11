cf1d2c76312b59382559d8cff92e310e956f41bc
Vetle Lindbråten <vetlelind@gmail.com>
2025-04-11 14:35:31 +0200

    moved extracting of answers to own file

---
+++ src/components/Summary/extractAnswers.ts
@@ -0,0 +1,124 @@
+import { useConfig } from "@context/Config";
+
+export default function extractAnswers(answers: any, survey: any, config: any) {
+    let newAnswers: any = {};
+    const questions: any[] = [];
+    
+    const getNestedQuestions = (elements: any[]) => {
+        let nestedQuestions: any[] = [];
+
+        elements.forEach((el) => { 
+            if (el.elements) {
+                nestedQuestions = nestedQuestions.concat(getNestedQuestions(el.elements));
+            } else {
+                nestedQuestions.push(el);
+            } 
+        });
+
+        return nestedQuestions;
+    };
+
+    survey.pages.forEach((page: any) => {
+        questions.push(...getNestedQuestions(page.elements));
+    });
+
+    questions.forEach((question: any) => {
+        if (answers[question.name] == undefined) { // No answer
+        } else if (question.type == "matrix" || question.type == "matrixdropdown") { // Matrix question, with rows and columns
+            let answer: { [key: string]: any } = {};
+
+            question.rows.map((row: any) => {
+                const rowName = row.name ?? row.value ?? row
+                answer[rowName] = {};
+
+                question.columns.map((column: any) => {
+                    const colName = column.name ?? column.value ?? column;
+                    const ans = answers[question.name]
+
+                    if (ans[rowName]?.[colName] || ans[rowName] == colName) {
+                        answer[rowName][colName] = ans[rowName][colName] ?? ans[rowName] == colName;
+                    }
+                })
+            })
+
+            newAnswers[question.name] = answer;
+        } else if (question.type == "matrixdynamic") { // Different from normal matrix
+            let answer: { [key: string]: any }[] = []; // TODO: Empty answer looking weird
+
+            answers[question.name].map((ans: any, index: number) => {
+                answer.push({});
+                Object.keys(ans).forEach((key: any) => {
+                    if (ans[key] == "other") {
+                        answer[index][key] = ans[`${key}-Comment`];
+                    } else if (key.endsWith("-Comment")) {
+                    } else {
+                        answer[index][key] = ans[key];
+                    }
+                })
+            })
+
+            newAnswers[question.name] = answer;
+        } else if (question.type == "file") { // Image (TODO: Consider adding more testing)
+            newAnswers[question.name] = answers[question.name];
+        } else if (question.type == "imagepicker") {
+            newAnswers[question.name] = question.choices.find((choice: any) => {
+                return choice.value === answers[question.name];
+            }).imageLink;
+        } else if (question.choices) { // Questions with choices (radio, checkbox, etc)
+            if (answers[question.name] == "other") { // Other and none does not show up as normal answers
+                newAnswers[question.name] = answers[`${question.name}-Comment`];     
+            } else if (answers[question.name] == "none") {
+                newAnswers[question.name] = config?.locale == "no" ? "Ingen" : "None";     
+            } else if (Array.isArray(answers[question.name])) { // Multiple answers
+                newAnswers[question.name] = answers[question.name].map((answer: any) => {
+                    const choice = question.choices.find((choice: any) => {
+                        if (typeof choice === "string") {
+                            return choice === answer;
+                        } else {
+                            return choice.value === answer;
+                        }
+                    });
+    
+                    return typeof choice === "string" ? choice : choice.text;
+                }).join(", ");
+            } else { // Normal answer
+                const choice = question.choices.find((choice: any) => {
+                    if (typeof choice === "string") {
+                        return choice === answers[question.name];
+                    } else {
+                        return choice.value === answers[question.name];
+                    }
+                });
+
+                newAnswers[question.name] = typeof choice === "string" ? choice : choice.text;
+            }
+        } else if (question.type == "text") {
+            if (question.inputType == "date") {
+                newAnswers[question.name] = new Date(answers[question.name]).toLocaleDateString(config?.locale, { year: 'numeric', month: '2-digit', day: '2-digit' });
+            } else if (question.inputType == "datetime-local") {
+                newAnswers[question.name] = new Date(answers[question.name]).toLocaleTimeString(config?.locale, { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' });
+            } else if (question.inputType == "week") { 
+                newAnswers[question.name] = (config?.locale === "no" ? "Uke " : "Week ") + answers[question.name].split("-W")[1] + ", " + answers[question.name].split("-W")[0];
+            } else {
+                newAnswers[question.name] = answers[question.name];
+            }
+        } else if (question.type == "boolean") {
+            newAnswers[question.name] = answers[question.name] ? (config?.locale == "no" ? "Ja" : "Yes") : (config?.locale == "no" ? "Nei" : "No");
+        } else if (question.type == "multipletext") {
+            let answer: { [key: string]: any }[] = [];
+
+            question.items.map((item: any) => {
+                answer.push({
+                    name: item.title ?? item.name ?? item,
+                    value: answers[question.name][item.name ?? item]
+                });
+            })
+
+            newAnswers[question.name] = answer;
+        } else { // Questions without choices (text, number, etc)
+            newAnswers[question.name] = answers[question.name];
+        }
+    });
+
+    return newAnswers;
+}
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,6 +1,7 @@
 import { useConfig } from "@context/Config";
 import { useMemo } from "react";
 import SummaryItem from "./SummaryItem";
+import extractAnswers from "./extractAnswers";
 
 const Summary: FC = () => {
     const config = useConfig();
@@ -12,124 +13,7 @@ const Summary: FC = () => {
     const answers = useMemo(() => {
         const jsonAnswers = JSON.parse(config.answerData || '{}');
 
-        let newAnswers: any = {};
-        const questions: any[] = [];
-        
-        const getNestedQuestions = (elements: any[]) => {
-            let nestedQuestions: any[] = [];
-    
-            elements.forEach((el) => { 
-                if (el.elements) {
-                    nestedQuestions = nestedQuestions.concat(getNestedQuestions(el.elements));
-                } else {
-                    nestedQuestions.push(el);
-                } 
-            });
-    
-            return nestedQuestions;
-        };
-
-        survey.pages.forEach((page: any) => {
-            questions.push(...getNestedQuestions(page.elements));
-        });
-
-        questions.forEach((question: any) => {
-            if (jsonAnswers[question.name] == undefined) { // No answer
-            } else if (question.type == "matrix" || question.type == "matrixdropdown") { // Matrix question, with rows and columns
-                let answer: { [key: string]: any } = {};
-
-                question.rows.map((row: any) => {
-                    const rowName = row.name ?? row.value ?? row
-                    answer[rowName] = {};
-
-                    question.columns.map((column: any) => {
-                        const colName = column.name ?? column.value ?? column;
-                        const ans = jsonAnswers[question.name]
-
-                        if (ans[rowName]?.[colName] || ans[rowName] == colName) {
-                            answer[rowName][colName] = ans[rowName][colName] ?? ans[rowName] == colName;
-                        }
-                    })
-                })
-
-                newAnswers[question.name] = answer;
-            } else if (question.type == "matrixdynamic") { // Different from normal matrix
-                let answer: { [key: string]: any }[] = []; // TODO: Empty answer looking weird
-
-                jsonAnswers[question.name].map((ans: any, index: number) => {
-                    answer.push({});
-                    Object.keys(ans).forEach((key: any) => {
-                        if (ans[key] == "other") {
-                            answer[index][key] = ans[`${key}-Comment`];
-                        } else if (key.endsWith("-Comment")) {
-                        } else {
-                            answer[index][key] = ans[key];
-                        }
-                    })
-                })
-
-                newAnswers[question.name] = answer;
-            } else if (question.type == "file") { // Image (TODO: Consider adding more testing)
-                newAnswers[question.name] = jsonAnswers[question.name];
-            } else if (question.type == "imagepicker") {
-                newAnswers[question.name] = question.choices.find((choice: any) => {
-                    return choice.value === jsonAnswers[question.name];
-                }).imageLink;
-            } else if (question.choices) { // Questions with choices (radio, checkbox, etc)
-                if (jsonAnswers[question.name] == "other") { // Other and none does not show up as normal answers
-                    newAnswers[question.name] = jsonAnswers[`${question.name}-Comment`];     
-                } else if (jsonAnswers[question.name] == "none") {
-                    newAnswers[question.name] = config.locale == "no" ? "Ingen" : "None";     
-                } else if (Array.isArray(jsonAnswers[question.name])) { // Multiple answers
-                    newAnswers[question.name] = jsonAnswers[question.name].map((answer: any) => {
-                        const choice = question.choices.find((choice: any) => {
-                            if (typeof choice === "string") {
-                                return choice === answer;
-                            } else {
-                                return choice.value === answer;
-                            }
-                        });
-        
-                        return typeof choice === "string" ? choice : choice.text;
-                    }).join(", ");
-                } else { // Normal answer
-                    const choice = question.choices.find((choice: any) => {
-                        if (typeof choice === "string") {
-                            return choice === jsonAnswers[question.name];
-                        } else {
-                            return choice.value === jsonAnswers[question.name];
-                        }
-                    });
-    
-                    newAnswers[question.name] = typeof choice === "string" ? choice : choice.text;
-                }
-            } else if (question.type == "text") {
-                if (question.inputType == "date") { // Date input
-                    newAnswers[question.name] = new Date(jsonAnswers[question.name]).toLocaleDateString(config.locale, { year: 'numeric', month: '2-digit', day: '2-digit' });
-                } else if (question.inputType == "datetime-local") { // Time input
-                    newAnswers[question.name] = new Date(jsonAnswers[question.name]).toLocaleTimeString(config.locale, { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' });
-                } else if (question.inputType == "week") { // Number input
-                    newAnswers[question.name] = (config.locale === "no" ? "Uke " : "Week ") + jsonAnswers[question.name].split("-W")[1] + ", " + jsonAnswers[question.name].split("-W")[0];
-                } else {
-                    newAnswers[question.name] = jsonAnswers[question.name];
-                }
-            } else if (question.type == "boolean") {
-                newAnswers[question.name] = jsonAnswers[question.name] ? (config.locale == "no" ? "Ja" : "Yes") : (config.locale == "no" ? "Nei" : "No");
-            } else if (question.type == "multipletext") {
-                let answer: { [key: string]: any }[] = [];
-
-                question.items.map((item: any) => {
-                    answer.push({
-                        name: item.title ?? item.name ?? item,
-                        value: jsonAnswers[question.name][item.name ?? item]
-                    });
-                })
-
-                newAnswers[question.name] = answer;
-            } else { // Questions without choices (text, number, etc)
-                newAnswers[question.name] = jsonAnswers[question.name];
-            }
-        });
+        const newAnswers = extractAnswers(jsonAnswers, survey, config);
 
         return newAnswers;
     }, [config.answerData, survey]);
