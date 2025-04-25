e6137708963d2399c41266e5f1de49c72c35d841
Vetle Lindbråten <vetlelind@gmail.com>
2025-04-25 10:10:11 +0200

    finished footer

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -43,90 +43,72 @@ const Summary: FC = () => {
         })}
         {config.footer && <div className="footer">
             {config.footer.top && <div className="footer-top">
-                <p>Pasientopplysninger og behandlingsrisiko</p>
-                <div className="footer-top-left">
-                    <div className="høyde-vekt">
-                        <div>
-                            <p className="høyde">Høyde: {config.footer.top.høyde}</p>
+                <p className="title">Pasientopplysninger og behandlingsrisiko</p>
+                <div className="content">
+                    <div className="footer-top-left">
+                        <div className="høyde-vekt">
+                            <p className="høyde">Høyde {config.footer.top.høyde}</p>
+                            <p className="vekt">Vekt {config.footer.top.vekt}</p>
                         </div>
-                        <div>
-                            <p className="vekt">Vekt: {config.footer.top.vekt}</p>
-                        </div>
-                    </div>
-                    <div className="skadeside-skadetype-gipslager">
-                        <div>
-                            <p className="skadeside">Skadeside: {config.footer.top.skadeside}</p>
-                        </div>
-                        <div>
-                            <p className="skadetype">Skadetype: {config.footer.top.skadetype}</p>
-                        </div>
-                        <div>
-                            <p className="gipslager">Gipslager: {config.footer.top.gipslager}</p>
+                        <div className="skadeside-skadetype-gipslager">
+                            <p className="skadeside">Skadeside {config.footer.top.skadeside}</p>
+                            <p className="skadetype">Skadetype {config.footer.top.skadetype}</p>
+                            <p className="gipslager">Gipslager {config.footer.top.gipslager}</p>
                         </div>
                     </div>
-                </div>
-                <div className="footer-top-right">
-                    <div>
+                    <div className="footer-top-right">
                         <p className="behandlingsrisiko">Beh. risiko {config.footer.top.behandlingsrisiko}</p>
-                    </div>
-                    <div>
                         <p className="kommunikasjonsbehov">Kom. behov {config.footer.top.kommunikasjonsbehov}</p>
-                    </div>
-                    <div>
                         <p className="sted">Sted: {config.footer.top.sted}</p>
                     </div>
                 </div>
             </div>}
             {config.footer.bottom && <div className="footer-bottom">
-                <p>Prøving og levering</p>
-                <div>
-                    <div>
+                <p className="title">Prøving og levering</p>
+                <div className="content">
+                    <div className="additional-info">
                         <p className="timeavtaler">Timeavtaler ordre {config.footer.bottom.timeavtaler}</p>
-                    </div>
-                    <div>
                         <p className="prøvedato">Prøvedato {config.footer.bottom.prøvedato}</p>
-                    </div>
-                    <div>
                         <p className="planlagtLevering">Planlagt levering {config.footer.bottom.planlagtLevering}</p>
                     </div>
-                </div>
-                <div className="checkboxes">
-                    <div className="checkboxes-top">
-                        <div className="checkbox ingeniør">
-                            <input type="checkbox" checked={config.footer.bottom.ingeniør} readOnly />
-                            <p>Ingeniør</p>
+                    <div className="checkboxes">
+                        <div className="checkboxes-top">
+                            <div className="checkbox ingeniør">
+                                <input type="checkbox" checked={config.footer.bottom.ingeniør} readOnly />
+                                <p>Ingeniør</p>
+                            </div>
+                            <div className="checkbox skoklinikk">
+                                <input type="checkbox" checked={config.footer.bottom.skoklinikk} readOnly />
+                                <p>Skoklinikk</p>
+                            </div>
                         </div>
-                        <div className="checkbox skoklinikk">
-                            <input type="checkbox" checked={config.footer.bottom.skoklinikk} readOnly />
-                            <p>Skoklinikk</p>
+                        <div className="checkbox send-post">
+                            <input type="checkbox" checked={config.footer.bottom.sendPost} readOnly />
+                            <p>Send med posten</p>
                         </div>
-                    </div>
-                    <div className="checkbox send-post">
-                        <input type="checkbox" checked={config.footer.bottom.sendPost} readOnly />
-                        <p>Send med posten</p>
-                    </div>
-                    <div className="checkboxes-middle">
-                        <div className="checkbox brev">
-                            <input type="checkbox" checked={config.footer.bottom.brev} readOnly />
-                            <p>Brev</p>
+                        <div className="checkboxes-middle">
+                            <div className="checkbox brev">
+                                <input type="checkbox" checked={config.footer.bottom.brev} readOnly />
+                                <p>Brev</p>
+                            </div>
+                            <div className="checkbox ring">
+                                <input type="checkbox" checked={config.footer.bottom.ring} readOnly />
+                                <p>Ringes</p>
+                            </div>
+                            <div className="checkbox sms">
+                                <input type="checkbox" checked={config.footer.bottom.sms} readOnly />
+                                <p>SMS</p>
+                            </div>
                         </div>
-                        <div className="checkbox ring">
-                            <input type="checkbox" checked={config.footer.bottom.ring} readOnly />
-                            <p>Ringes</p>
-                        </div>
-                        <div className="checkbox sms">
-                            <input type="checkbox" checked={config.footer.bottom.sms} readOnly />
-                            <p>SMS</p>
-                        </div>
-                    </div>
-                    <div className="checkboxes-bottom">
-                        <div className="checkbox innlevert-luka">
-                            <input type="checkbox" checked={config.footer.bottom.innlevertLuka} readOnly />
-                            <p>Innlevert i luka</p>
-                        </div>
-                        <div className="checkbox tilsendt-post">
-                            <input type="checkbox" checked={config.footer.bottom.tilsendtPost} readOnly />
-                            <p>Tilsendt per post</p>
+                        <div className="checkboxes-bottom">
+                            <div className="checkbox innlevert-luka">
+                                <input type="checkbox" checked={config.footer.bottom.innlevertLuka} readOnly />
+                                <p>Innlevert i luka</p>
+                            </div>
+                            <div className="checkbox tilsendt-post">
+                                <input type="checkbox" checked={config.footer.bottom.tilsendtPost} readOnly />
+                                <p>Tilsendt per post</p>
+                            </div>
                         </div>
                     </div>
                 </div>
