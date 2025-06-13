cf80d96af557e146b94b3fe7e1a0aeb5d6133ec6
Vetle Lindbråten <vetlelind@gmail.com>
2025-06-13 15:37:39 +0200

    fixes styling and formatting

--- src/components/Summary/History.tsx
+++ src/components/Summary/History.tsx
@@ -22,59 +22,67 @@ export const HistoryItem: FC<{ answerHistory: any, elementName: string }> = ({ a
         };
 
         document.addEventListener("mousedown", handleClick);
-
-        return () => {
-            document.removeEventListener("mousedown", handleClick);
-        };
+        return () => document.removeEventListener("mousedown", handleClick);
     }, [answerHistoryOpen]);
 
-    if (!(answerHistory.some((item: any) => item.answers[elementName] != undefined))) return null;
+    if (answerHistory.every((item: any) => item.answers[elementName] == undefined)) return null;
 
-    return <div className={"answer-history" + (answerHistoryOpen ? " open" : "")} ref={itemRef}>
-        {!answerHistoryOpen ? (
-            <button className="answer-history__open" onClick={() => setAnswerHistoryOpen(true)}>
-                <History />
-            </button>
-        ) : (
-            <div className="answer-history__panel">
-                <button className="answer-history__button" onClick={() => setAnswerHistoryOpen(false)}>
-                    {config?.locale === "no" ? "Lukk" : "Close"}
+    return ( 
+        <div 
+            className={"answer-history" + (answerHistoryOpen ? " open" : "")} 
+            ref={itemRef}
+        >
+            {!answerHistoryOpen ? (
+                <button 
+                    className="answer-history__open" 
+                    onClick={() => setAnswerHistoryOpen(true)}
+                >
+                    <History />
                 </button>
-                <ul>
-                    {answerHistory.map((history: any, index: number) => {
-                        const answer = history.answers[elementName];
-                        const next = answerHistory[index + 1]?.answers[elementName];
-                        
-                        if (answer === next) return null;
+            ) : (
+                <div className="answer-history__panel">
+                    <button 
+                        className="answer-history__button" 
+                        onClick={() => setAnswerHistoryOpen(false)}
+                    >
+                        {config?.locale === "no" ? "Lukk" : "Close"}
+                    </button>
+                    <ul>
+                        {answerHistory.map((history: any, index: number) => {
+                            const answer = history.answers[elementName];
+                            const next = answerHistory[index + 1]?.answers[elementName];
+                            
+                            if (answer === next) return null;
 
-                        return (
-                            <li key={index}>
-                                <span>{history.user}</span>
-                                <span>•</span>
-                                <span>
-                                    {answer ?? (config?.locale === 'no' ? 'slettet svaret' : 'deleted answer')}
-                                </span>
-                                {history.timestamp && (
-                                    <>
-                                        <span>•</span>
-                                        <span className="time-ago">
-                                            {new Date(history.timestamp).toLocaleString(
-                                                config?.locale === 'no' ? 'nb-NO' : 'en-US',
-                                                {
-                                                    dateStyle: 'short',
-                                                    timeStyle: 'short',
-                                                }
-                                            )}
-                                        </span>
-                                    </>
-                                )}
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-        )}
-    </div>
+                            return (
+                                <li key={index}>
+                                    <span>{history.user}</span>
+                                    <span>•</span>
+                                    <span>
+                                        {answer ?? (config?.locale === 'no' ? 'slettet svaret' : 'deleted answer')}
+                                    </span>
+                                    {history.timestamp && (
+                                        <>
+                                            <span>•</span>
+                                            <span className="time-ago">
+                                                {new Date(history.timestamp).toLocaleString(
+                                                    config?.locale === 'no' ? 'nb-NO' : 'en-US',
+                                                    {
+                                                        dateStyle: 'short',
+                                                        timeStyle: 'short',
+                                                    }
+                                                )}
+                                            </span>
+                                        </>
+                                    )}
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export const HistoryList: FC<{ sortedHistory: any }> = ({ sortedHistory }) => {
@@ -86,103 +94,105 @@ export const HistoryList: FC<{ sortedHistory: any }> = ({ sortedHistory }) => {
 
     if (!config) return null;
 
-    return <div className="change-history">
-        {!historyOpen ? (
-            <button
-                className="change-history__badge"
-                aria-label="Show change history"
-                onClick={() => setHistoryOpen(!historyOpen)}
-            >
-                {config.locale === "no" ? "Endringshistorikk" : "Changes history"} ({sortedHistory.length})
-            </button>
-        ) : (
-            <div className="change-history__panel">
-                <div className="change-history__buttons">
-                    <button
-                        className="change-history__button"
-                        onClick={() => {
-                            setHistoryOpen(false);
-                            setViewingChanges(false);
-                            setAnswerData(null);
-                            setActiveIndex(null);
-
-                            const newAnswerData = answerData || config.answerData;
-                            setConfig((prev): any => ({
-                                ...prev,
-                                answerData: newAnswerData,
-                            }));
-                        }}
-                    >
-                        {config.locale === 'no' ? 'Lukk' : 'Close'}
-                    </button>
-                    {viewingChanges && (
-                        <>
-                            <button
-                                className="change-history__button"
-                                onClick={() => {
-                                    setViewingChanges(false);
-                                    setAnswerData(null);
-                                    setActiveIndex(null);
-
-                                    const newAnswerData = answerData || config.answerData;
-                                    setConfig((prev): any => ({
-                                        ...prev,
-                                        answerData: newAnswerData,
-                                    }));
-                                }}
-                            >
-                                {config.locale === 'no' ? 'Stopp visning av endringer' : 'Stop viewing changes'}
-                            </button>
-                            <button
-                                className="change-history__button"
-                                onClick={() => {
-                                    if (config.scriptNames?.onChange) {
-                                        performScript("onChange", {
-                                            result: JSON.parse(config.answerData || '{}'),
-                                            hasErrors: false,
-                                        });
-                                    }
-                                    setViewingChanges(false);
-                                    setAnswerData(null);
-                                    setHistoryOpen(false);
-                                    setActiveIndex(null);
-                                }}
-                            >
-                                {config.locale === 'no' ? 'Lagre versjon' : 'Save version'}
-                            </button>
-                        </>
-                    )}
-                </div>
-                <ul>
-                    {sortedHistory.map((h: any, index: number) => (
-                        <li
-                            className={(activeIndex === index) ? 'active' : ''}
-                            key={index}
+    return (
+        <div className="change-history">
+            {!historyOpen ? (
+                <button
+                    className="change-history__badge"
+                    aria-label="Show change history"
+                    onClick={() => setHistoryOpen(!historyOpen)}
+                >
+                    {config.locale === "no" ? "Endringshistorikk" : "Changes history"} ({sortedHistory.length})
+                </button>
+            ) : (
+                <div className="change-history__panel">
+                    <div className="change-history__buttons">
+                        <button
+                            className="change-history__button"
                             onClick={() => {
-                                !answerData && setAnswerData(config.answerData || null);
+                                setHistoryOpen(false);
+                                setViewingChanges(false);
+                                setAnswerData(null);
+                                setActiveIndex(null);
+
+                                const newAnswerData = answerData || config.answerData;
                                 setConfig((prev): any => ({
                                     ...prev,
-                                    answerData: h.answer,
+                                    answerData: newAnswerData,
                                 }));
-                                setViewingChanges(true);
-                                setActiveIndex(index);
                             }}
                         >
-                            <div className="meta">
-                                <span className="user">{h.user}</span>{' '}
-                                {h.timestamp && (
-                                    <span className="timestamp">
-                                        {new Date(h.timestamp).toLocaleString(config.locale || 'en-US', {
-                                            dateStyle: 'short',
-                                            timeStyle: 'short',
-                                        })}
-                                    </span>
-                                )}
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        )}
-    </div>
+                            {config.locale === 'no' ? 'Lukk' : 'Close'}
+                        </button>
+                        {viewingChanges && (
+                            <>
+                                <button
+                                    className="change-history__button"
+                                    onClick={() => {
+                                        setViewingChanges(false);
+                                        setAnswerData(null);
+                                        setActiveIndex(null);
+
+                                        const newAnswerData = answerData || config.answerData;
+                                        setConfig((prev): any => ({
+                                            ...prev,
+                                            answerData: newAnswerData,
+                                        }));
+                                    }}
+                                >
+                                    {config.locale === 'no' ? 'Stopp visning av endringer' : 'Stop viewing changes'}
+                                </button>
+                                <button
+                                    className="change-history__button"
+                                    onClick={() => {
+                                        if (config.scriptNames?.onChange) {
+                                            performScript("onChange", {
+                                                result: JSON.parse(config.answerData || '{}'),
+                                                hasErrors: false,
+                                            });
+                                        }
+                                        setViewingChanges(false);
+                                        setAnswerData(null);
+                                        setHistoryOpen(false);
+                                        setActiveIndex(null);
+                                    }}
+                                >
+                                    {config.locale === 'no' ? 'Lagre versjon' : 'Save version'}
+                                </button>
+                            </>
+                        )}
+                    </div>
+                    <ul>
+                        {sortedHistory.map((h: any, index: number) => (
+                            <li
+                                className={(activeIndex === index) ? 'active' : ''}
+                                key={index}
+                                onClick={() => {
+                                    !answerData && setAnswerData(config.answerData || null);
+                                    setConfig((prev): any => ({
+                                        ...prev,
+                                        answerData: h.answer,
+                                    }));
+                                    setViewingChanges(true);
+                                    setActiveIndex(index);
+                                }}
+                            >
+                                <div className="meta">
+                                    <span className="user">{h.user}</span>{' '}
+                                    {h.timestamp && (
+                                        <span className="timestamp">
+                                            {new Date(h.timestamp).toLocaleString(config.locale || 'en-US', {
+                                                dateStyle: 'short',
+                                                timeStyle: 'short',
+                                            })}
+                                        </span>
+                                    )}
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
 }
