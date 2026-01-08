8fbaec8f68864b23e843f296b1154537d6a96b35
Vetle Lindbråten <vetlelind@gmail.com>
2026-01-08 15:21:08 +0100

    Config comments

--- src/types/config.d.ts
+++ src/types/config.d.ts
@@ -1,24 +1,28 @@
 declare global {
 	namespace Form {
 		interface Config {
-			// The form data, in json format
+			/** The form data, in json format */
 			value?: string;
 
-			// The current answers to the form, updated along the way.
-			// Use to continue where the user left off
+			/**
+			 * The current answers to the form, updated along the way.
+			 * Use to continue where the user left off
+			 */
 			answerData?: string;
 
-			// If the form should be compact or not
+			/** If the form should be compact or not */
 			compact?: boolean;
 
-			// If some questions should be validated from FileMaker
-			// Must have the validate script name in the scriptNames object
+			/**
+			 * If some questions should be validated from FileMaker
+			 * Must have the validate script name in the scriptNames object
+			 */
 			validateFromFileMaker?: boolean;
 
-			// Hides the button to complete the form
+			/** Hides the button to complete the form */
 			hideCompleteButton?: boolean;
 
-			// The language of the form, can add more if needed
+			/** The language of the form, can add more if needed */
 			locale: 'no' | 'en';
 
 			scriptNames?: Form.ScriptNames;
@@ -28,7 +32,7 @@ declare global {
 		}
 	}
 
-	// Make values accessible via window
+	/** Make values accessible via window */
 	interface Window {
 		_config?: Form.Config;
 	}
